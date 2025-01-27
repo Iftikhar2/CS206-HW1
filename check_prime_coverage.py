b3ef3fbd4e098aa8e87e896af4c3109322d0ffb9
Iftikhar2 <140280663+Iftikhar2@users.noreply.github.com>
2025-01-27 15:32:50 -0800

    Add files via upload

--- check_prime_coverage.py
+++ check_prime_coverage.py
@@ -10,9 +10,18 @@ cov.start()
 import sys
 sys.path.append(sys.path[0] + "/..")
 
-from tests.check_prime import checkPrime
+from check_prime import checkPrime
 
-num_entered = int(input("Enter a number : "))
+while True:
+    try:
+        num_entered = int(input("Enter an integer greater than 1: "))
+        if num_entered < 2:
+            raise ValueError("Enter an integer greater than 1:")  # Raise a ValueError if age is negative
+        break  # Exit the loop if input is valid
+    except ValueError:
+        print("Invalid input. Please enter an integer greater than 1.")
+
+# num_entered = int(input("Enter an integer : "))
 result = checkPrime(num_entered)
 print(result)
 
