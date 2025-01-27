5074f3c84ce9017da2b95482c363c65281366ac9
Iftikhar2 <140280663+Iftikhar2@users.noreply.github.com>
2025-01-27 15:40:52 -0800

    Add files via upload

--- check_prime_coverage.py
+++ check_prime_coverage.py
@@ -16,12 +16,11 @@ while True:
     try:
         num_entered = int(input("Enter an integer greater than 1: "))
         if num_entered < 2:
-            raise ValueError("Enter an integer greater than 1:")  # Raise a ValueError if age is negative
+            raise ValueError("Enter an integer greater than 1:")
         break  # Exit the loop if input is valid
     except ValueError:
         print("Invalid input. Please enter an integer greater than 1.")
-
-# num_entered = int(input("Enter an integer : "))
+        
 result = checkPrime(num_entered)
 print(result)
 
